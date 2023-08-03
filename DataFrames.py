0f2a03b180b653ada982a7c10ccd4614554d1f99
luciancristialx <alexandruluciancristi@gmail.com>
2023-08-03 10:32:07 +0300

    Video training - 161, 162 + Read CSV, Testing iterations through DataFrames

--- DataFrames.py
+++ DataFrames.py
@@ -10,17 +10,35 @@ E = [7,8,9,0]
 #Define dataframe
 df = pd.DataFrame(data = [A,B,C,D,E],index = ['a','b','c','d','e'], columns = ['W', 'X', 'Y', 'Z'])
 print("DataFrame:\n\n", df,"\n--------------------------------\n")
-print("Print element from first column and first row: ",df['W'][0],"\n--------------------------------\n")
+print("Print element from first column and first row: ",df.loc['a','W'],"\n--------------------------------\n")
+print("Print first row:\n", df.loc['a'],"\n--------------------------------\n")
+print("Print first row (numeric indexes):\n", df.iloc[0],"\n--------------------------------\n")
 
-print("Print elements in first column:")
-for rowIndex in df.index:
-    print(df['W'][rowIndex])
-print("--------------------------------")
+#Conditional selection
+print("Conditional selection:\n",df > 3,"\n--------------------------------\n")
 
-print("Read data from CSV and format output as following:\n")
-ReportData = pd.read_csv("TestingFile.csv")
-# print(ReportData)
 
-for i in range(len(ReportData)):
-    print(ReportData.iloc[i,0],'->',ReportData.iloc[i,0],'->',ReportData.iloc[i,3])
-print("--------------------------------")
+# print("Print elements in first column:")
+# for rowIndex in df.index:
+#     print(df['W'][rowIndex])
+# print("--------------------------------")
+
+#Add column
+# df['P'] = df['Y']+df['Z']
+# print("DataFrame + new column:\n\n", df,"\n--------------------------------\n")
+
+#Remove row (axis 0) - temporary (inplace False) / permanently
+# df.drop('e', inplace = True)
+# print("DataFrame - row e removed:\n\n", df,"\n--------------------------------\n")
+
+#Remove column (axis 1) - temporary (inplace False) / permanently
+# df.drop('P', axis = 1 , inplace = True)
+# print("DataFrame - column P removed:\n\n", df,"\n--------------------------------\n")
+
+# print("Read data from CSV and format output as following:\n")
+# ReportData = pd.read_csv("TestingFile.csv")
+# # print(ReportData)
+#
+# for i in range(len(ReportData)):
+#     print(ReportData.iloc[i,0],'->',ReportData.iloc[i,0],'->',ReportData.iloc[i,3])
+# print("--------------------------------")
